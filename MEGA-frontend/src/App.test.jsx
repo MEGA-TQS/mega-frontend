e32d96afd75a3e70425bdce2fd580521d1763c0e
gRibau <goncalob.ribau@gmail.com>
2025-12-13 19:59:28 +0000

    mocked navbar in test

--- MEGA-frontend/src/App.test.jsx
+++ MEGA-frontend/src/App.test.jsx
@@ -1,10 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react';
-import App from './App'; 
+import { describe, it, expect, vi } from 'vitest';
+import App from './App';
 import '@testing-library/jest-dom';
 
-test('renders the main application wrapper', () => {
-  render(<App />);
-  const element = screen.getByText(/Rent Gear. Go Outside./i); 
-  expect(element).toBeInTheDocument();
+vi.mock('./components/Navbar', () => ({
+  default: () => <div>Mock Navigation Bar</div>
+}));
+
+describe('App Component', () => {
+  it('renders the main application wrapper', () => {
+    render(<App />);
+    expect(screen.getByText('Mock Navigation Bar')).toBeInTheDocument();
+  });
 });
